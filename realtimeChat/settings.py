a57b5c206994dddb5c47653946c73201a2cfe940
keroti <peterkeroti55@gmail.com>
2023-11-15 18:40:30 +0300

    Csrf added

--- realtimeChat/settings.py
+++ realtimeChat/settings.py
@@ -131,3 +131,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS=['chatappkeroti.azurewebsites.net']
